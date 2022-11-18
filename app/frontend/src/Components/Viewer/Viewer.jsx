bec96fba47d3372f5ef8ed1c9453a3251808f433
lil-sahil <sahilnagpal93@yahoo.ca>
2022-11-17 22:57:01 -0500

    Added css dependency for pdf viewer to properly display pdf file.

--- app/frontend/src/Components/Viewer/Viewer.jsx
+++ app/frontend/src/Components/Viewer/Viewer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const options = {
