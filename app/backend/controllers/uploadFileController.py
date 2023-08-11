5341b51a4afe003737d94dbe11e900b59722081a
lil-sahil <sahilnagpal93@yahoo.ca>
2023-08-11 11:12:55 -0400

    Added a check to ensure that the derived part number exists in the the database.

--- app/backend/controllers/uploadFileController.py
+++ app/backend/controllers/uploadFileController.py
@@ -8,74 +8,93 @@ class Clean_File:
         self.file_name = file_name
         print(self.file_name)
         self.df = self.load_pdf_file()
+        self.part_number_df = pd.read_csv("./srPartNumbers.csv")
 
     def column_names(self):
         return [
-                "Quantity on Hand", 
-                "Quantity Allocated", 
-                "Quantity on Order", 
-                "Quantity on P.O", 
-                "Minimum Quantity", 
-                "Maximum Quantity",
-                "Part #",
-                "Description",
-                "Quantity Type",
-                "Net Quantity"
+            "Quantity on Hand",
+            "Quantity Allocated",
+            "Quantity on Order",
+            "Quantity on P.O",
+            "Minimum Quantity",
+            "Maximum Quantity",
+            "Part #",
+            "Description",
+            "Quantity Type",
+            "Net Quantity"
         ]
-    
+
     def load_pdf_file(self):
         return tabula.read_pdf(self.file_name, pages='all', multiple_tables=False)[0]
-
 
     def format_vals(self, val_array):
         formatted_vals_arr = []
 
         for val in val_array:
-            # Check if a string contains two decimal points, 
+            # Check if a string contains two decimal points,
             # if so, split the string into two, everything before and after the second decimal point.
-            if(str(val).count(".") > 1):
-                formatted_vals_arr.append(".".join(val.split(".",2)[:-1]))
-            
+            if (str(val).count(".") > 1):
+                formatted_vals_arr.append(".".join(val.split(".", 2)[:-1]))
+
             # Turn the unnamed value into a nan.
             elif ("unnamed" in str(val).lower()):
                 formatted_vals_arr.append(np.nan)
 
             else:
                 formatted_vals_arr.append(val)
-        
+
         return formatted_vals_arr
-        
-        
-    def extract_description(self,x1, x2):
+
+    def extract_description(self, x1, x2):
         # Check if the description already exists in the x2 column.
         # If it does then return it.
         # If it does not then the description is in the x1 col.
         # Return the x1 col minus the description.
-        
+
         if type(x2) == str:
             return x2
-        
+
         else:
             return x1[7:]
-        
+
+    def check_if_part_number_exists(self, part_number):
+        return True if self.part_number_df[self.part_number_df['LQPART'] == part_number].shape[0] == 1 else False
+
     def check_sr_number(self, x1, x2):
-        # If the last character is a letter in sr number
-        if(x1[-1].isalpha()):
-            return x1[:-1], x1[-1] + x2
+
+        if (self.check_if_part_number_exists(x1)):
+            return x1, x2
+
+        # If the last character is a letter is a sr number
+        if (x1[-1].isalpha()):
+
+            if (self.check_if_part_number_exists(x1[:-1])):
+                return x1[:-1], x1[-1] + x2
 
         # If the sr number starts with RSR
-        if(x1[0] == "R"):
-            return x1 + x2[0], x2[1:]
-        
+        if (x1[0] == "R"):
+            if (self.check_if_part_number_exists(x1 + x2[0])):
+                return x1 + x2[0], x2[1:]
+
         else:
-            return x1,x2
+            # Start to delete the last character and reference the db
+            for i in range(1, 3):
+                if (self.check_if_part_number_exists(x1[:i*-1])):
+                    return x1[:i*-1], x1[i*-1:] + x2
+
+            # Start to add the number from the description and reference the db
+            for i in range(0, 3):
+                if (self.check_if_part_number_exists(x1+x2[i])):
+                    return x1+x2[i], x2[i+1:]
+
+        return x1, x2
 
     def clean_df(self):
         print("I am here")
         # Make column_name into a row at the very end of the df.
         self.df.loc[-1] = self.df.columns
-        self.df.loc[-1,2] = np.nan
-        
+        self.df.loc[-1, 2] = np.nan
+
         # Clean the last row to format values such as .00.2
         print(self.df.iloc[-1].values)
         self.df.loc[-1] = self.format_vals(self.df.iloc[-1].values)
@@ -87,49 +106,53 @@ class Clean_File:
         # example "DW24824668KF50HD183 #3 STEEL STAMP" => "DW24824"
         self.df['Part #'] = self.df[1].apply(lambda x: x[0:7])
 
-
         # Make "Description Column"
-        self.df['Description'] = self.df[[1,2]].apply(lambda x: self.extract_description(x[1], x[2]), axis = 1 )
+        self.df['Description'] = self.df[[1, 2]].apply(
+            lambda x: self.extract_description(x[1], x[2]), axis=1)
 
         # Drop columns 1 and 2.
-        self.df.drop(axis =1, columns = [1,2], inplace=True)
+        self.df.drop(axis=1, columns=[1, 2], inplace=True)
 
         # Make "Quantity Type" column
         # example "1.00 EA" => "EA"
-        self.df['Quantity Type'] = self.df[9].apply(lambda x: str(x).split(" ")[-1])
+        self.df['Quantity Type'] = self.df[9].apply(
+            lambda x: str(x).split(" ")[-1])
 
         # Make "Net Quantity" column
         # example "1.00 EA" => "1.00"
-        self.df['Net Quantity'] = self.df[9].apply(lambda x: str(x).split(" ")[0])
+        self.df['Net Quantity'] = self.df[9].apply(
+            lambda x: str(x).split(" ")[0])
 
         # Drop columns 9 and 10
-        self.df.drop(axis = 1, columns=[9,10], inplace=True)
+        self.df.drop(axis=1, columns=[9, 10], inplace=True)
 
         # Rename columns to final names
         self.df.columns = self.column_names()
 
         # Reorder columns
-        self.df = self.df.loc[:, ["Part #", 
-                                  "Description", 
-                                  "Quantity on Hand", 
-                                  "Quantity Allocated", 
-                                  "Quantity on Order", 
-                                  "Quantity on P.O", 
-                                  "Minimum Quantity", 
+        self.df = self.df.loc[:, ["Part #",
+                                  "Description",
+                                  "Quantity on Hand",
+                                  "Quantity Allocated",
+                                  "Quantity on Order",
+                                  "Quantity on P.O",
+                                  "Minimum Quantity",
                                   "Maximum Quantity",
                                   "Net Quantity",
                                   "Quantity Type"]]
 
         # Drop rows with nan values for the "Quantity on hand" column
-        self.df.dropna(subset=["Quantity on Hand"], inplace = True)
+        self.df.dropna(subset=["Quantity on Hand"], inplace=True)
 
-        #Reorder the rows so -1 index comes to the top.
-        self.df = pd.concat([self.df[self.df.index == -1], self.df[self.df.index != -1]])
-        
+        # Reorder the rows so -1 index comes to the top.
+        self.df = pd.concat([self.df[self.df.index == -1],
+                            self.df[self.df.index != -1]])
+
         # Reset the index, so it start from 0.
         self.df.reset_index(drop=True, inplace=True)
 
-        self.df[["Part #", "Description"]] = self.df[["Part #", "Description"]].apply(lambda x: pd.Series(self.check_sr_number(x[0], x[1])), axis=1)
+        self.df[["Part #", "Description"]] = self.df[["Part #", "Description"]].apply(
+            lambda x: pd.Series(self.check_sr_number(x[0], x[1])), axis=1)
 
         print("Finished!")
 
